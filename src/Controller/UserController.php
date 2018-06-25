5200bd4d602c2a107c1e38b2d0d6cf53253e07c3
SimonClerbout <simon.clerbout@laposte.net>
2018-06-25 14:30:03 +0200

    Revert stuff wtf

--- src/Controller/UserController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Controller;
-
-class UserController extends Controller {
-
-
-
-}
--- src/Entity/User.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 25/06/18
- * Time: 11:44
- */
-
